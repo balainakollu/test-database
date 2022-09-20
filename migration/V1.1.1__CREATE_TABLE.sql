48ff783c3fa9e8046242d5546ec17194b3c0b7e4
balainakollu <bala.inakollu@blinxsolutions.com>
2022-09-20 09:25:45 +0100

    Intial Load

---
+++ migration/V1.1.1__CREATE_TABLE.sql
@@ -0,0 +1,11 @@
+create table staging.stg_snomed_codes_test
+(
+coding_id varchar(50),
+cluster_id varchar(50),
+cluster_description varchar(500),
+snomed_concept_id varchar(50),
+code_description varchar(500),
+type_of_inclusion varchar(50),
+included_under varchar(50),
+active_status varchar(50)
+);
